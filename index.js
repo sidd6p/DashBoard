45d91f66f8b828d57f86510e3a70b0324e0dd76e
Siddhartha <siddpurwar@gmail.com>
2022-05-15 23:37:59 +0530

    All function are working

--- index.js
+++ index.js
@@ -1,12 +1,82 @@
+
+const authorEl = document.getElementById('author');
+const timeEl = document.getElementById("time");
+const weatherEl = document.getElementById("weather");
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         // throw Error("This is error");
-        console.log(data.urls.regular);
         document.body.style.background  = `url(${data.urls.regular})`;
-        document.getElementById('author').textContent = `By: ${data.user.username}`;
+        authorEl.textContent = `By: ${data.user.username}`;
     })
     .catch(error => {
         document.body.style.background = `url('https://images.unsplash.com/photo-1444465693019-aa0b6392460d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTI2MTkyMTM&ixlib=rb-1.2.1&q=80&w=1080')`;
         // console.log(error);
     });
+
+
+function getweather(latitude, longitude) {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`)
+        .then(res => res.json())
+        .then(data => {
+            weatherEl.innerHTML = `
+                                    <div>
+                                        Temperature: ${data.main.temp} C
+                                    </div>
+                                    <div>
+                                        Humidity: ${data.main.humidity} %
+                                    </div>
+                                    <div>
+                                        Pressure: ${data.main.pressure} hPa
+                                    </div>
+                                    <div>
+                                        Wind: ${data.wind.speed} m/s
+                                    </div>
+                                `
+        })
+}
+
+if ('geolocation' in navigator) {
+    const options = {
+        enableHighAccuracy: true,
+    }
+    navigator.geolocation.getCurrentPosition(position => {
+        getweather(position.coords.latitude, position.coords.longitude);
+    }, (error) => {
+        // console.log(error.code);
+        weatherEl.innerText = error.message;
+    },  options);
+}   
+else {
+    weatherEl.innerText = 'Unable to Access Location';
+}
+
+function getTime() {
+    const date = new Date();
+    timeEl.innerHTML = `
+        ${date.toLocaleTimeString('en-us', {timeStyle: 'medium'})}
+    `
+}
+
+setInterval(getTime, 1000);
+
+
+// console.log(date);
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleTimeString('en-us', {timeStyle: 'short'}));
+// fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+//     .then(res => {
+//         if (!res.ok) {
+//             throw Error("404 Error");
+//         }
+//         return res.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
